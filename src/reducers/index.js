48308258e87169ba9abe2d7e2e70a892854a2d68
tavo <gasb_150@hotmail.com>
2021-05-25 17:49:17 -0500

    Create app

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from "redux"
+
+const songsReducer = () => {
+  return [
+    {title: 'No scrubs', duration: '4:05'},
+    {title: 'Macarena', duration: '2:30'},
+    {title: 'All start', duration: '3:15'},
+    {title: 'I want in that way', duration: '1:45'}
+
+  ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) =>{
+  if (action.type === 'SONG_SELECTED'){
+    return action.payload
+  }
+
+  return selectedSong
+}
+
+export default combineReducers ({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
